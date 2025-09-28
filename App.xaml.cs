e7bc192995059da79272fcefd36085f4e1267e02
avi.kupitman@gmail.com <avi.kupitman@gmail.com>
2025-09-28 19:32:44 +0300

    I added an Login and Register pages that function

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,8 +5,8 @@
         public App()
         {
             InitializeComponent();
-
             MainPage = new AppShell();
+            Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
         }
     }
 }
